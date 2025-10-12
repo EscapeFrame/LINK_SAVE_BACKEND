0904b871bd803a19eeded511ca5617895455bb05
Haeyul <kimminje661@outlook.kr>
2025-10-12 19:54:44 +0900

    init

---
+++ src/main/java/clue/link_save/LinkSaveApplication.java
@@ -0,0 +1,13 @@
+package clue.link_save;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkSaveApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LinkSaveApplication.class, args);
+  }
+
+}
---
+++ src/test/java/clue/link_save/LinkSaveApplicationTests.java
@@ -0,0 +1,13 @@
+package clue.link_save;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkSaveApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
