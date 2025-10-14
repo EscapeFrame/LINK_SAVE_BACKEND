97669efca85c7fec6187f0d0f636d27bbe37d02d
Haeyul <kimminje661@outlook.kr>
2025-10-14 23:53:11 +0900

    feat(#2): 불필요한 추상화 메서드 삭제

--- src/main/java/clue/link_save/domain/link/persistence/LinkRepository.java
+++ src/main/java/clue/link_save/domain/link/persistence/LinkRepository.java
@@ -1,15 +1,11 @@
 package clue.link_save.domain.link.persistence;
 
-import clue.link_save.domain.link.domain.Authorization;
+import clue.link_save.domain.link.domain.AuthorizationType;
 import clue.link_save.domain.link.domain.Link;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface LinkRepository extends JpaRepository<Link, Long> {
-  List<Link> findByGradeAndClas(char grade, char clas);
-
-  Page<Link> findByGradeAndClasAndAuthorization(char grade, char clas, Authorization authorization, Pageable pageable);
+  Page<Link> findByGradeAndClasAndAuthorizationType(char grade, char clas, AuthorizationType authorizationType, Pageable pageable);
 }
